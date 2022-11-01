918f27669836724e81b1f9f3a1d8196efffc776e
David <david.santos54@fatec.sp.gov.br>
2022-11-01 19:21:37 -0300

    Criação do Projeto

---
+++ src/main/java/com/solbs/unoapi/UnoApiApplication.java
@@ -0,0 +1,13 @@
+package com.solbs.unoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solbs/unoapi/UnoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solbs.unoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
