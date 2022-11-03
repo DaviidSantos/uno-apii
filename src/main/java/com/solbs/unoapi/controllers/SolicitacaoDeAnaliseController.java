9c20f81cc11e268123b04a0efff3fde97aa39189
David <david.santos54@fatec.sp.gov.br>
2022-11-03 13:12:36 -0300

    Solicitação de Análise Controller finalizado

--- src/main/java/com/solbs/unoapi/controllers/SolicitacaoDeAnaliseController.java
+++ src/main/java/com/solbs/unoapi/controllers/SolicitacaoDeAnaliseController.java
@@ -22,12 +22,32 @@ public class SolicitacaoDeAnaliseController {
     @Autowired
     private SolicitanteService solicitanteService;
 
+    /**
+     * Método HTTP que retorna uma lista de Solicitações de Análise
+     * @return Entidade de resposta com a lista de Solicitações de Análise
+     */
     @GetMapping
     public ResponseEntity<List<SolicitacaoDeAnalise>> retornarTodosSolicitacaoDeAnalises(){
         List<SolicitacaoDeAnalise> lista = solicitacaoDeAnaliseService.retornarTodasSolicitacoesDeAnalise();
         return ResponseEntity.ok().body(lista);
     }
 
+    /**
+     * Método HTTP que retorna uma Solicitação de Análise a partir de seu id
+     * @param idSA Id da Solicitação de Análise a ser retornada
+     * @return Entidade de resposta com a solicitação de análise retornada
+     */
+    @GetMapping("/{idSA}")
+    public ResponseEntity<SolicitacaoDeAnalise> retornarSolicitacaoDeAnalisePorId(@PathVariable Long idSA){
+        SolicitacaoDeAnalise solicitacaoDeAnalise = solicitacaoDeAnaliseService.retornarSolicitacaoDeAnalisePorId(idSA);
+        return ResponseEntity.status(HttpStatus.OK).body(solicitacaoDeAnalise);
+    }
+
+    /**
+     * Método HTTP que cadastra uma Solicitação de Análise na base de dados
+     * @param solicitacaoDeAnaliseDTO Dados da Solicitação de Análise a ser cadastrada
+     * @return Entidade de resposta com a solicitação de análise cadastrada
+     */
     @PostMapping
     public ResponseEntity<SolicitacaoDeAnalise> cadastrarSolicitacaoDeAnalise(@RequestBody SolicitacaoDeAnaliseDTO solicitacaoDeAnaliseDTO){
         SolicitacaoDeAnalise solicitacaoDeAnalise = new SolicitacaoDeAnalise();
@@ -36,5 +56,18 @@ public class SolicitacaoDeAnaliseController {
         solicitante = solicitanteService.retornarSolicitantePorCnpj(solicitacaoDeAnaliseDTO.getCnpj());
         solicitacaoDeAnalise.setSolicitante(solicitante);
         return ResponseEntity.status(HttpStatus.CREATED).body(solicitacaoDeAnaliseService.cadastrarSolicitacaoDeAnalise(solicitacaoDeAnalise));
+    }
+
+    /**
+     * Método HTTP que atualiza dados da Solicitação de Análise
+     * @param idSA id da Solicitação que será atualizada
+     * @param solicitacaoDeAnaliseDTO dados que serão atualizados na Solicitação de Análise
+     * @return Entidade de resposta com a Solicitação de Análise atualizada
+     */
+    @PutMapping("/{idSA}")
+    public ResponseEntity<SolicitacaoDeAnalise> atualizarSolicitacaoDeAnalise(@PathVariable Long idSA, @RequestBody SolicitacaoDeAnaliseDTO solicitacaoDeAnaliseDTO){
+        SolicitacaoDeAnalise solicitacaoDeAnalise = solicitacaoDeAnaliseService.retornarSolicitacaoDeAnalisePorId(idSA);
+        solicitacaoDeAnalise = solicitacaoDeAnaliseService.atualizarDados(solicitacaoDeAnalise, solicitacaoDeAnaliseDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(solicitacaoDeAnaliseService.cadastrarSolicitacaoDeAnalise(solicitacaoDeAnalise));
     }
 }
--- src/main/java/com/solbs/unoapi/services/SolicitacaoDeAnaliseService.java
+++ src/main/java/com/solbs/unoapi/services/SolicitacaoDeAnaliseService.java
@@ -1,5 +1,6 @@
 package com.solbs.unoapi.services;
 
+import com.solbs.unoapi.dtos.SolicitacaoDeAnaliseDTO;
 import com.solbs.unoapi.entities.SolicitacaoDeAnalise;
 import com.solbs.unoapi.repositories.SolicitacaoDeAnaliseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +13,34 @@ public class SolicitacaoDeAnaliseService {
     @Autowired
     private SolicitacaoDeAnaliseRepository solicitacaoDeAnaliseRepository;
 
+    @Autowired
+    private SolicitanteService solicitanteService;
+
     public SolicitacaoDeAnalise cadastrarSolicitacaoDeAnalise(SolicitacaoDeAnalise solicitacaoDeAnalise){
         return solicitacaoDeAnaliseRepository.save(solicitacaoDeAnalise);
     }
 
     public List<SolicitacaoDeAnalise> retornarTodasSolicitacoesDeAnalise(){
         return solicitacaoDeAnaliseRepository.findAll();
+    }
+
+    public SolicitacaoDeAnalise retornarSolicitacaoDeAnalisePorId(Long idSA) {
+        return solicitacaoDeAnaliseRepository.findById(idSA).get();
+    }
+
+    public SolicitacaoDeAnalise atualizarDados(SolicitacaoDeAnalise solicitacaoDeAnalise, SolicitacaoDeAnaliseDTO solicitacaoDeAnaliseDTO) {
+        if(solicitacaoDeAnaliseDTO.getTipoDeAnalise() != null){
+            solicitacaoDeAnalise.setTipoDeAnalise(solicitacaoDeAnaliseDTO.getTipoDeAnalise());
+        }
+
+        if(solicitacaoDeAnaliseDTO.getConsideracoesGerais() != null){
+            solicitacaoDeAnalise.setConsideracoesGerais(solicitacaoDeAnaliseDTO.getConsideracoesGerais());
+        }
+
+        if (solicitacaoDeAnaliseDTO.getInformacoesGerais() != null){
+            solicitacaoDeAnalise.setInformacoesGerais(solicitacaoDeAnaliseDTO.getInformacoesGerais());
+        }
+
+        return solicitacaoDeAnalise;
     }
 }
